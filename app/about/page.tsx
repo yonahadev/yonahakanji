e1fc6ef09764714e556ad53dcfe5be5b109e7ae7
tom <83908974+goat6@users.noreply.github.com>
2023-08-02 11:34:35 +0100

    Added about section

---
+++ app/about/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-center opacity-75">
+      <p>
+        Thanks for checking out my website. This is a simple kanji dictionary in
+        a familiar style for Japanese learners looking to find new Kanji or
+        remember existing ones.
+      </p>
+      <br></br>
+      <p>
+        This site uses the{" "}
+        <Link
+          className="underline text-accent"
+          href={"https://foosoft.net/projects/yomichan/index.html#dictionaries"}
+        >
+          KANJIDIC
+        </Link>{" "}
+        dictionary which contains numerous pieces of additional data which can
+        be used to identify each kanji. The site has a total of 10350 kanji
+        ranging from the most used to lesser used ones.
+      </p>
+      <br></br>
+      <p>
+        Each kanji has information regarding its Kunyomi and Onyomi readings as
+        well as the English translation. You can search with either the English
+        Japanese character sets including Romaji, Hiragana, Katakana and Kanji.
+        The current system ranks search results by newspaper frequency to try
+        and estimate relevance, although I would be willing to listen to any
+        suggestions to try and improve this.
+      </p>
+      <br></br>
+      <p>
+        If you have any issues or would like to look at more of my work, feel
+        free to click my github link below to find me. Happy learning!
+      </p>
+    </div>
+  );
+};
+
+export default page;
